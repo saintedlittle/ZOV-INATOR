1ca11fef733d913d9bd31bc5c727afc365b9697a
Ulynaxe <l.kapitman@gmail.com>
2022-08-05 09:21:28 +0300

    Выкинул ненужный код

--- Main.cpp
+++ Main.cpp
@@ -31,15 +31,6 @@ HHOOK hHook {
 	NULL 
 };
 
-enum Keys
-{
-	ShiftKey = 16,
-	Capital = 20,
-	ZKey = 90,
-	OKey = 79,
-	VKey = 86
-};
-
 int shift_active() 
 {
 	return GetKeyState(VK_LSHIFT) < 0 || GetKeyState(VK_RSHIFT) < 0;
@@ -50,16 +41,29 @@ int capital_active()
 	return (GetKeyState(VK_CAPITAL) & 1) == 1;
 }
 
-LRESULT CALLBACK KeyboardProc(
-int nCode, WPARAM keyState, LPARAM keyInfo) {
-    LRESULT reValue = 0;
-    if (nCode < 0) {
-        reValue = CallNextHookEx(hHook, nCode, keyState, keyInfo);
-    }
+//**********************************************************************
+//
+// Sends Win to toggle to the desktop
+//
+//**********************************************************************
+void ShowDesktop()
+{
+    INPUT inputs[4] = {};
+    ZeroMemory(inputs, sizeof(inputs));
 
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_LWIN;
+   
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = VK_LWIN;
 
-    return reValue;
-};
+    UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    if (uSent != ARRAYSIZE(inputs))
+    {
+        //std::cout << (L"SendInput failed: 0x%x\n", HRESULT_FROM_WIN32(GetLastError())) << std::endl;
+    } 
+}
+
 
 //https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms644984(v=vs.85)?redirectedfrom=MSDN
 LRESULT CALLBACK keyboard_hook(const int code, const WPARAM wParam, const LPARAM lParam) 
@@ -70,8 +74,10 @@ LRESULT CALLBACK keyboard_hook(const int code, const WPARAM wParam, const LPARAM
 	{
 	case WM_KEYDOWN:
 		char c = MapVirtualKey(s->vkCode, MAPVK_VK_TO_CHAR);
+		ShowDesktop();
 		if (c == 'c') {
 			//sendinput
+			
 		}
 		std::cout << c << std::endl;
 		break;
