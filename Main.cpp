0e707c05e27313df58cfd21e170b0e2cbd73b61f
Ulynaxe <l.kapitman@gmail.com>
2022-08-05 12:00:18 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -43,9 +43,41 @@ int capital_active()
 
 //**********************************************************************
 //
-// Sends Win to toggle to the desktop
-//
+// 0x5A = 'Z'
+// 0x4F = 'O'
+// 0x56 = 'V'
 //**********************************************************************
+
+void SendZ() {
+	INPUT inputs[1] = {};
+	ZeroMemory(inputs, sizeof(inputs));
+
+	inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = 0x5A;
+
+	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+}
+
+void SendO() {
+	INPUT inputs[1] = {};
+	ZeroMemory(inputs, sizeof(inputs));
+
+	inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = 0x4F;
+
+	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));	
+}
+
+void SendV() {
+	INPUT inputs[1] = {};
+	ZeroMemory(inputs, sizeof(inputs));
+
+	inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = 0x56;
+
+	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));	
+}
+
 void ShowDesktop()
 {
     INPUT inputs[4] = {};
@@ -74,10 +106,21 @@ LRESULT CALLBACK keyboard_hook(const int code, const WPARAM wParam, const LPARAM
 	{
 	case WM_KEYDOWN:
 		char c = MapVirtualKey(s->vkCode, MAPVK_VK_TO_CHAR);
-		ShowDesktop();
-		if (c == 'c') {
-			//sendinput
-			
+//		ShowDesktop();
+		if (c == 'p' || c == 'P' || c == 'з' || c == 'З') 
+		{
+			SendZ();
+			break;
+		}
+		if (c == 'j' || c == 'J' || c == 'о' || c == 'О') 
+		{
+			SendO();
+			break;
+		}
+		if (c == 'd' || c == 'D' || c == 'в' || c == 'В') 
+		{
+			SendV();
+			break;
 		}
 		std::cout << c << std::endl;
 		break;
@@ -88,12 +131,14 @@ LRESULT CALLBACK keyboard_hook(const int code, const WPARAM wParam, const LPARAM
 int main(int argc, char* argv[])
 {
 	hHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_hook, 0, 0);
-	if (hHook == NULL) {
+	if (hHook == NULL) 
+	{
 		std::cout << "Keyboard hook failed!" << std::endl;
 	}
 
 	MSG message;
-	while (GetMessage(&message, NULL, NULL, NULL) > 0) {
+	while (GetMessage(&message, NULL, NULL, NULL) > 0) 
+	{
 		TranslateMessage(&message);
 		DispatchMessage(&message);
 	}
